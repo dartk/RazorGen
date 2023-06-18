7d58fa8f3639ebb1680ef46bae177497c3504419
dartk <dartk@users.noreply.github.com>
2023-06-19 03:16:25 +0700

    Adds support for external references

--- CSharp.SourceGen.Razor/RazorIncrementalGenerator.cs
+++ CSharp.SourceGen.Razor/RazorIncrementalGenerator.cs
@@ -15,13 +15,43 @@ public class RazorIncrementalGenerator : IIncrementalGenerator
     public RazorIncrementalGenerator()
     {
         this._razorEngine = new RazorEngine();
+
+        var netstandardAssembly = Assembly.Load(
+            "netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51");
+        var mscorlibAssembly = typeof(object).Assembly;
+
+        this._netstandardAssemblies = netstandardAssembly.GetReferencedAssemblies()
+            .Select(Assembly.Load)
+            .Append(netstandardAssembly)
+            .Append(mscorlibAssembly)
+            .ToImmutableArray();
+
+        this._netstandardAssemblyReferences =
+            this._netstandardAssemblies.Select(GetReferenceFromAssembly).ToImmutableArray();
     }
 
 
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        var referencesProvider = context.CompilationProvider.Select((compilation, _) =>
-            compilation.References.ToImmutableArray());
+        var referencesProvider = context.CompilationProvider
+            .SelectMany((compilation, _) => compilation.References.ToImmutableArray())
+            .Select((reference, _) =>
+            {
+                if (reference.Display == null) return null;
+
+                try
+                {
+                    var assembly = Assembly.LoadFile(reference.Display);
+                    var fullName = assembly.FullName;
+                    return new ReferenceInfo(fullName, reference);
+                }
+                catch
+                {
+                    return null;
+                }
+            })
+            .Where(x => x != null)
+            .Collect();
 
         var additionalFiles = context.AdditionalTextsProvider
             .Select(static (file, token) =>
@@ -41,23 +71,49 @@ public class RazorIncrementalGenerator : IIncrementalGenerator
 
         var provider = razorTemplates.Combine(referencesProvider);
 
-        context.RegisterSourceOutput(provider, GenerateSources);
+        context.RegisterSourceOutput(provider, this.GenerateSources!);
     }
 
 
-    public static void GenerateSources(SourceProductionContext context,
-        (ImmutableArray<RazorTemplateSyntaxTree>, ImmutableArray<MetadataReference>) arg)
+    private void GenerateSources(SourceProductionContext context,
+        (ImmutableArray<RazorTemplateSyntaxTree>, ImmutableArray<ReferenceInfo>) arg)
     {
+        var (templates, projectReferences) = arg;
+        var referenceByName =
+            projectReferences.ToDictionary(x => x.AssemblyName, x => x.Reference);
+
+        Assembly ResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            if (referenceByName.TryGetValue(args.Name, out var reference))
+            {
+                var assembly = Assembly.LoadFrom(reference.Display!);
+                return assembly;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
+
         try
         {
-            var (templates, references) = arg;
+            var references = this._netstandardAssemblyReferences.Concat(
+                projectReferences.Select(x => x.Reference));
 
             var assemblyBytes = RazorEngine.EmitAssembly(templates.Select(x => x.SyntaxTree),
-                references, context.ReportDiagnostic);
+                references!, context.ReportDiagnostic);
+            if (assemblyBytes == null)
+            {
+                return;
+            }
 
-            var assembly = Assembly.Load(assemblyBytes);
+            var assembly = Assembly.Load(assemblyBytes)
+                ?? throw new NullReferenceException("Assembly is not loaded");
+
             var resultMethod = GetResultMethod(assembly)
-                ?? throw new ArgumentException("Result method not found");
+                ?? throw new NullReferenceException("Result method not found");
 
             foreach (var template in templates)
             {
@@ -68,15 +124,22 @@ public class RazorIncrementalGenerator : IIncrementalGenerator
                 context.AddSource(template.TypeFullName, renderedText);
             }
 
+
             static MethodInfo? GetResultMethod(Assembly assembly)
             {
-                var type = assembly.GetType("TemplateBase");
+                const string typeName = "TemplateBase";
+                var type = assembly.GetType(typeName)
+                    ?? throw new NullReferenceException($"Type '{typeName}' was not found");
                 return type.GetMethod("Result")!;
             }
         }
         catch (Exception ex)
         {
             context.ReportDiagnostic(Diagnostic.Create(Error, Location.None, ex.ToString()));
+        }
+        finally
+        {
+            AppDomain.CurrentDomain.AssemblyResolve -= ResolveAssembly;
         }
     }
 
@@ -90,6 +153,17 @@ public class RazorIncrementalGenerator : IIncrementalGenerator
         isEnabledByDefault: true);
 
 
+    private readonly ImmutableArray<Assembly> _netstandardAssemblies;
+    private readonly ImmutableArray<MetadataReference> _netstandardAssemblyReferences;
+
+
     private const string DiagnosticIdPrefix = "SourceGen.Razor";
     private const string DiagnosticCategory = "CSharp.SourceGen.Razor";
+
+
+    private static MetadataReference GetReferenceFromAssembly(Assembly assembly) =>
+        MetadataReference.CreateFromFile(assembly.Location);
+
+
+    private record ReferenceInfo(string AssemblyName, MetadataReference Reference);
 }
---
+++ Test2/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Test2;
+
+
+public class Class1
+{
+    public static void Foo()
+    {
+        Console.WriteLine(ScribanRazorMadness.Hello());
+    }
+}
